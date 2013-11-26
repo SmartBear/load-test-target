91515473700adb939856f05a4e68d90bd76319c0
Renato Athaydes <renato.deathaydes@smartbear.com>
2013-11-26 17:00:52 +0100

    Added some json random responses

---
+++ src/test/java/com/smartbear/loadtest/dao/PreFabricatedResponsesTest.java
@@ -0,0 +1,24 @@
+package com.smartbear.loadtest.dao;
+
+import org.junit.Test;
+
+/**
+ * @author renato
+ */
+public class PreFabricatedResponsesTest
+{
+
+	@Test
+	public void firstCycleResponsesShouldHaveAFixedResponseDelay() {
+
+	}
+
+	public void secondCycleResponsesShouldHaveADifferentDelay() {
+
+	}
+
+	public void afterSecondCycle_ResponsesShouldHaveAnotherDelay() {
+
+	}
+
+}
